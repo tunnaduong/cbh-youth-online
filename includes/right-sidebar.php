e266b9be571caef3e75f6ece409c9c604b143891
Tunna Duong <tunnaduong@gmail.com>
2023-11-19 10:55:57 +0700

    ok

---
+++ includes/right-sidebar.php
@@ -0,0 +1,76 @@
+<!-- Right side bar -->
+<div class="w-60 p-5 h-min sticky top-[48px]" id="right-sidebar">
+    <button class="mb-2 text-[11px] font-semibold bg-[#319527] flex items-center justify-center w-[100%] text-left leading-3 text-white rounded-lg p-1.5">
+        <ion-icon name="add-outline" class="text-[16px] mr-1"></ion-icon>
+        Tạo bài viết mới
+    </button>
+    <div class="bg-white text-[11px] p-2.5 mt-4 rounded-lg long-shadow">
+        <span class="font-bold text-[#6B6B6B] block">Xếp hạng tháng</span>
+        <div class="flex flex-row items-center mt-2">
+            <span class="w-6 h-6 text-[10px] text-white bg-gray-300 rounded-full flex items-center justify-center">K63</span>
+            <span class="flex flex-1 ml-1.5 font-semibold">12 Nga</span>
+            <span class="mr-1.5 text-[#C1C1C1]">-12 điểm</span>
+            <ion-icon name="trophy" class="text-[15px] text-yellow-400"></ion-icon>
+        </div>
+        <div class="flex flex-row items-center mt-2">
+            <span class="w-6 h-6 text-[10px] text-white bg-gray-300 rounded-full flex items-center justify-center">K64</span>
+            <span class="flex flex-1 ml-1.5 font-semibold">11 Toán</span>
+            <span class="mr-1.5 text-[#C1C1C1]">-6 điểm</span>
+            <ion-icon name="trophy" class="text-[15px] text-yellow-400"></ion-icon>
+        </div>
+        <div class="flex flex-row items-center mt-2">
+            <span class="w-6 h-6 text-[10px] text-white bg-gray-300 rounded-full flex items-center justify-center">K65</span>
+            <span class="flex flex-1 ml-1.5 font-semibold">10 Sử</span>
+            <span class="mr-1.5 text-[#C1C1C1]">-18 điểm</span>
+            <ion-icon name="trophy" class="text-[15px] text-yellow-400"></ion-icon>
+        </div>
+        <div class="flex flex-row items-center mt-2">
+            <span class="w-6 h-6 text-[10px] text-white bg-gray-300 rounded-full flex items-center justify-center">K9</span>
+            <span class="flex flex-1 ml-1.5 font-semibold">9A1</span>
+            <span class="mr-1.5 text-[#C1C1C1]">-4 điểm</span>
+            <ion-icon name="trophy" class="text-[15px] text-yellow-400"></ion-icon>
+        </div>
+        <div class="flex flex-row items-center mt-2">
+            <span class="w-6 h-6 text-[10px] text-white bg-gray-300 rounded-full flex items-center justify-center">K10</span>
+            <span class="flex flex-1 ml-1.5 font-semibold">8A2</span>
+            <span class="mr-1.5 text-[#C1C1C1]">-6 điểm</span>
+            <ion-icon name="trophy" class="text-[15px] text-yellow-400"></ion-icon>
+        </div>
+        <div class="flex flex-row items-center mt-2">
+            <span class="w-6 h-6 text-[10px] text-white bg-gray-300 rounded-full flex items-center justify-center">K11</span>
+            <span class="flex flex-1 ml-1.5 font-semibold">7A1</span>
+            <span class="mr-1.5 text-[#C1C1C1]">-10 điểm</span>
+            <ion-icon name="trophy" class="text-[15px] text-yellow-400"></ion-icon>
+        </div>
+        <div class="flex flex-row items-center mt-2">
+            <span class="w-6 h-6 text-[10px] text-white bg-gray-300 rounded-full flex items-center justify-center">K12</span>
+            <span class="flex flex-1 ml-1.5 font-semibold">6A2</span>
+            <span class="mr-1.5 text-[#C1C1C1]">-14 điểm</span>
+            <ion-icon name="trophy" class="text-[15px] text-yellow-400"></ion-icon>
+        </div>
+        <hr class="my-2" />
+        <div class="flex flex-row items-center">
+            <span class="w-6 h-6 text-[10px] text-white bg-gray-300 rounded-full flex items-center justify-center">K63</span>
+            <span class="flex flex-1 ml-1.5 font-semibold">Lớp bạn</span>
+            <span class="mr-1.5 text-[#C1C1C1]">-35 điểm</span>
+            <span class="text-green-500 font-bold">#3</span>
+        </div>
+    </div>
+    <div class="flex flex-row text-[10px] font-semibold p-3 text-[#BCBCBC]">
+        <div class="flex flex-1 flex-col gap-y-0.5">
+            <a href="#">Hỗ trợ</a>
+            <a href="#">Liên hệ</a>
+            <a href="#">Blog</a>
+            <a href="#">Quảng cáo</a>
+        </div>
+        <div class="flex flex-1 flex-col ml-5 gap-y-0.5">
+            <a href="#">Giới thiệu</a>
+            <a href="#">Việc làm</a>
+            <a href="#">Điều khoản</a>
+            <a href="#">Quyền riêng tư</a>
+        </div>
+    </div>
+    <p class="text-[8.5px] text-center text-[#BCBCBC]">
+        Fatties Software © 2022
+    </p>
+</div>
---
+++ views/404.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+include_once './includes/head.php';
+?>
+
+<body class="bg-[#F8F8F8] mt-12">
+  <?php
+  include_once './includes/navbar.php';
+  ?>
+  <!-- Main body -->
+  <div class="flex flex-1">
+
+  </div>
+  <script src="./static/js/script.js"></script>
+</body>
+
+</html>
