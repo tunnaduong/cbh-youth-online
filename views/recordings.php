4228e15c339abd5e4e6de9272dd070728f57ecb5
Tunna Duong <tunnaduong@gmail.com>
2023-11-25 14:48:56 +0700

    Update recording styles

--- views/recordings.php
+++ views/recordings.php
@@ -18,8 +18,8 @@ include_once './includes/head.php';
     ?>
     <!-- Main body -->
     <div class="flex flex-1 p-5 items-center flex-col">
-      <!-- Section 1 -->
-      <div class="max-w-lg w-[100%] mb-6 flex flex-row rounded-md bg-white">
+      <!-- Recording 1 -->
+      <div class="max-w-lg w-[100%] long-shadow mb-4 flex flex-row rounded-md bg-white">
         <div class="flex items-center justify-center mr-2">
           <img src="/static/img/soundwaves.png" alt="Soundwaves" class="w-14">
           <img class="absolute w-6" src="/static/img/play.png" alt="Play button">
@@ -36,9 +36,84 @@ include_once './includes/head.php';
             <span>2 ngày trước</span>
             <div class="flex items-center flex-1 flex-row-reverse mr-2.5">
               <span>46</span>
-              <ion-icon name="eye-outline"></ion-icon>
+              <ion-icon class="text-[15px] mr-1 ml-2" name="eye-outline"></ion-icon>
               <span>05+</span>
-              <ion-icon name="chatbox-outline"></ion-icon>
+              <ion-icon class="text-[15px] mr-1" name="chatbox-outline"></ion-icon>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Recording 2 -->
+      <div class="max-w-lg w-[100%] long-shadow mb-4 flex flex-row rounded-md bg-white">
+        <div class="flex items-center justify-center mr-2">
+          <img src="/static/img/dj.png" alt="DJ" class="w-14">
+          <img class="absolute w-6" src="/static/img/play.png" alt="Play button">
+        </div>
+        <div class="flex flex-col flex-1">
+          <div class="flex flex-row flex-1 items-center mt-[2px]">
+            <h1 class="flex-1 text-[14.5px] font-semibold">Nhạc DJ Remix bay lắc cực xung</h1>
+            <span class="text-[10px] mr-3 text-gray-500">01:23:03</span>
+          </div>
+          <div class="flex flex-1 items-center text-[10px] text-gray-500">
+            <img src="/static/img/tunganh.png" alt="tunnaduong" class="mr-1.5 w-5 h-5 rounded-full">
+            <span>Đăng bởi <span class="font-semibold text-[#319527]">Dương Tùng Anh</span></span>
+            <span class="mb-2 mx-0.5 text-sm text-gray-500">.</span>
+            <span>2 ngày trước</span>
+            <div class="flex items-center flex-1 flex-row-reverse mr-2.5">
+              <span>372</span>
+              <ion-icon class="text-[15px] mr-1 ml-2" name="eye-outline"></ion-icon>
+              <span>25+</span>
+              <ion-icon class="text-[15px] mr-1" name="chatbox-outline"></ion-icon>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Recording 3 -->
+      <div class="max-w-lg w-[100%] long-shadow mb-4 flex flex-row rounded-md bg-white">
+        <div class="flex items-center justify-center mr-2">
+          <img src="/static/img/ghoststory.png" alt="Soundwaves" class="w-14">
+          <img class="absolute w-6" src="/static/img/play.png" alt="Play button">
+        </div>
+        <div class="flex flex-col flex-1">
+          <div class="flex flex-row flex-1 items-center mt-[2px]">
+            <h1 class="flex-1 text-[14.5px] font-semibold">Chuyện ma lúc 3 giờ sáng</h1>
+            <span class="text-[10px] mr-3 text-gray-500">44:17</span>
+          </div>
+          <div class="flex flex-1 items-center text-[10px] text-gray-500">
+            <img src="/static/img/ngocngan.png" alt="Ngoc Ngan" class="mr-1.5 w-5 h-5 rounded-full">
+            <span>Đăng bởi <span class="font-semibold text-[#319527]">Bác Nguyễn Ngọc Ngạn</span></span>
+            <span class="mb-2 mx-0.5 text-sm text-gray-500">.</span>
+            <span>2 ngày trước</span>
+            <div class="flex items-center flex-1 flex-row-reverse mr-2.5">
+              <span>85</span>
+              <ion-icon class="text-[15px] mr-1 ml-2" name="eye-outline"></ion-icon>
+              <span>10+</span>
+              <ion-icon class="text-[15px] mr-1" name="chatbox-outline"></ion-icon>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Recording 4 -->
+      <div class="max-w-lg w-[100%] long-shadow mb-4 flex flex-row rounded-md bg-white">
+        <div class="flex items-center justify-center mr-2">
+          <img src="/static/img/soundwaves.png" alt="Soundwaves" class="w-14">
+          <img class="absolute w-6" src="/static/img/play.png" alt="Play button">
+        </div>
+        <div class="flex flex-col flex-1">
+          <div class="flex flex-row flex-1 items-center mt-[2px]">
+            <h1 class="flex-1 text-[14.5px] font-semibold">Thông báo Đoàn ngày 28/03/2022</h1>
+            <span class="text-[10px] mr-3 text-gray-500">05:42</span>
+          </div>
+          <div class="flex flex-1 items-center text-[10px] text-gray-500">
+            <img src="/static/img/cbh.jpeg" alt="CBH" class="mr-1.5 w-5 h-5 rounded-full">
+            <span>Đăng bởi <span class="font-semibold text-[#319527]">Đoàn Trường Biên Hòa</span></span>
+            <span class="mb-2 mx-0.5 text-sm text-gray-500">.</span>
+            <span>2 ngày trước</span>
+            <div class="flex items-center flex-1 flex-row-reverse mr-2.5">
+              <span>32</span>
+              <ion-icon class="text-[15px] mr-1 ml-2" name="eye-outline"></ion-icon>
+              <span>0</span>
+              <ion-icon class="text-[15px] mr-1" name="chatbox-outline"></ion-icon>
             </div>
           </div>
         </div>
