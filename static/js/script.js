aab5e501432280817d5bfe1a298f3a26f061ec77
Tunna Duong <tunnaduong@gmail.com>
2023-11-29 10:30:23 +0700

    Fix navigation active class bug

--- static/js/script.js
+++ static/js/script.js
@@ -31,7 +31,9 @@ document.addEventListener("DOMContentLoaded", function () {
   for (var i = 0; i < btns.length; i++) {
     btns[i].addEventListener("click", function () {
       var current = document.getElementsByClassName("nav-active");
-      current[0].className = current[0].className.replace(" nav-active", "");
+      if (current.length != 0) {
+        current[0].className = current[0].className.replace(" nav-active", "");
+      }
       //   this.className += " nav-active";
     });
   }
